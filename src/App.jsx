40bbf0fd3b40a44f89def1e8f3d81f04a33e9247
Stakshi <semwaleishta6@gmail.com>
2025-04-10 16:38:36 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {Routes,Route} from "react-router-dom"
-import { Navigate } from "react-router-dom";
-import Home from "./Home"
-import About from "./About"
-import Contact from "./Contact"
-import Service from "./Service"
-import Navbar from "./Navbar"
+import { Routes, Route, Navigate } from "react-router-dom";
 
-const App=()=>(
-  <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/service" element={<Service/>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  </>
-);
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Service from "./Service";
+import Navbar from "./Navbar";
+
+const App = () => {
+
+  return (
+    <>
+<Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,102 +1,70 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './index.css'
+import './index.css';
 import { FaSun, FaMoon } from "react-icons/fa";
-import { useDispatch,useSelector } from "react-redux";
-import {toggleTheme} from './redux/Slice';
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from './redux/Slice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const isDarkTheme = useSelector((state) => state.theme.Theme);
 
-  const dispatch=useDispatch();
-  const selector=useSelector((state)=>state.theme.Theme)
-
-  function handleToggle(){
-dispatch(toggleTheme());
-document.body.classList.toggle('dark-mode');
-  }
+  const handleToggle = () => {
+    dispatch(toggleTheme());
+    document.body.classList.toggle('dark-mode');
+  };
 
   return (
-    <>
-      <div className="container-fluid nav_bg">
-        <div className="row">
-          <div className="col-10 mx-auto">
-            <nav className={`navbar navbar-expand-lg border-custom my-2 ${selector ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
-              <div className="container-fluid">
-                <NavLink className="navbar-brand gradient-text" to="/">
-                  DevLTops
-                </NavLink>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarSupportedContent"
-                >
-
-<div className="text-end w-100">
-  <button className="btn btn-outline-dark py-1 px-2" onClick={handleToggle}>
-    {selector ? <FaSun /> : <FaMoon />}
-  </button>
-</div>
-
-
-                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ml">
-                    <li className="nav-item">
-                      <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                          isActive ? "nav-link menu_active" : "nav-link"
-                        }
-                      >
-                        Home
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink
-                        to="/service"
-                        className={({ isActive }) =>
-                          isActive ? "nav-link menu_active" : "nav-link"
-                        }
-                      >
-                        Service
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink
-                        to="/about"
-                        className={({ isActive }) =>
-                          isActive ? "nav-link menu_active" : "nav-link"
-                        }
-                      >
-                        About
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink
-                        to="/contact"
-                        className={({ isActive }) =>
-                          isActive ? "nav-link menu_active" : "nav-link"
-                        }
-                      >
-                        Contact
-                      </NavLink>
-                    </li>
-                  </ul>
+    <div className="container-fluid nav_bg">
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <nav className={`navbar navbar-expand-lg border-custom my-2 ${isDarkTheme ? 'navbar-dark' : 'navbar-light'}`}>
+            <div className="container-fluid">
+              <NavLink className="navbar-brand gradient-text" to="/">
+                DevLTops
+              </NavLink>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="text-end w-100 mb-2 mb-lg-0">
+                  <button
+                    className={`btn ${isDarkTheme ? 'btn-outline-light' : 'btn-outline-dark'} py-1 px-2`}
+                    onClick={handleToggle}
+                  >
+                    {isDarkTheme ? <FaSun /> : <FaMoon />}
+                  </button>
                 </div>
+
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/service" className="nav-link">Service</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/about" className="nav-link">About</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
+                  </li>
+                </ul>
+
               </div>
-            </nav>
-          </div>
+            </div>
+          </nav>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
